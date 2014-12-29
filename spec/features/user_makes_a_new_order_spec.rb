46d40372895bee5836ef3f2197ce5a1db95be5f8
graffam <graffam87@gmail.com>
2014-12-28 19:40:38 -0500

    added more information in the user_adds_an_order spec file

--- spec/features/user_makes_a_new_order_spec.rb
+++ spec/features/user_makes_a_new_order_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper.rb'
+# As a user I want to be able to place a new order
+# After a new order is I should be redirected to the order itself
+# The order should display information such as user who placed it etc...
+#  [X] User can visit the new order page and be shown a list of machines
+#  [] User can place a new order
+#  [] User is redirected to the order they just placed
 
 feature "User adds a new order" do
 
@@ -10,16 +16,18 @@ feature "User adds a new order" do
     click_on "Log in"
     click_on "New Order"
     click_on "HS2500"
+    expect(page).to have_content()
   end
 
-  # scenario "user places an order" do
-  #   user = FactoryGirl.new(:user)
-  #   visit new_user_session_path
-  #   fill_in "Email", with: user.email
-  #   fill_in "Password", with: user.password
-  #   click_on "Log In"
-  #   visit new_order_path
-  # end
+  scenario "user places a new order" do
+    user = FactoryGirl.create(:user)
+    visit new_user_session_path
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_on "Log in"
+    click_on "New Order"
+    click_on "HS2500"
+  end
 
 
 
